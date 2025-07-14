9fd61e30522fae5f1c6f6ec7c4280d036ea0f5ec
vijaykishore_a <70621045+Vijay-Kishore-A@users.noreply.github.com>
2025-07-14 18:40:36 -0400

    Create wait-for-db.sh

---
+++ wait-for-db.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+echo "Waiting for PostgreSQL at $host..."
+
+until pg_isready -h "$host" -U "postgres"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
